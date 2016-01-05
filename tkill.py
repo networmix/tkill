43e5f4bbf6e42ac7e86c183c001da90eaf785b7f
golova <golova@amazon.com>
2016-01-05 17:33:14 +0000

    Version 1.0.1. Minor improvement in exception handling.

--- tkill.py
+++ tkill.py
@@ -7,7 +7,8 @@ import signal
 import os
 import sys
 
-__version__ = "1.0"
+
+__version__ = "1.0.1"
 __status__ = "First release"
 
 
@@ -20,37 +21,43 @@ SIG = signal.SIGTERM
 
 
 def main():
-	get_pid_cmd = "ps -ax"
-	get_ifaces_cmd = "ifconfig -lu"
-	
-	while True:
-		proc_lst = subprocess.check_output(get_pid_cmd, shell=True).split('\n')
-		ifaces = subprocess.check_output(get_ifaces_cmd, shell=True)
-		
-		pids = []
-		# Getting all pids belonging to 'APPNAME'
-		for string in proc_lst:
-			if APPNAME in string:
-				pids.append(string.split()[0]) 
+    get_pid_cmd = "ps -ax"
+    get_ifaces_cmd = "ifconfig -lu"
+    
+    while True:
+        proc_lst = subprocess.check_output(get_pid_cmd, shell=True).split('\n')
+        ifaces = subprocess.check_output(get_ifaces_cmd, shell=True)
+        
+        pids = []
+        # Getting all pids belonging to 'APPNAME'
+        for string in proc_lst:
+            if APPNAME in string:
+                pids.append(string.split()[0]) 
 
-		VPN_ACTIVE = True if IFNAME in ifaces else False
-		UT_ACTIVE = True if pids else False
+        VPN_ACTIVE = True if IFNAME in ifaces else False
+        UT_ACTIVE = True if pids else False
 
-		if UT_ACTIVE and VPN_ACTIVE:
-			time.sleep(1)
+        if UT_ACTIVE and VPN_ACTIVE:
+            time.sleep(1)
 
-		# We don't allow to run 'APPNAME' if VPN ('IFNAME') is not there
-		elif UT_ACTIVE:
-			for pid in pids:
-				os.kill(int(pid), SIG)
-			time.sleep(1)
+        # We don't allow to run 'APPNAME' if VPN ('IFNAME') is not there
+        elif UT_ACTIVE:
+            for pid in pids:
+                try:
+                    os.kill(int(pid), SIG)
+                except:
+                    formatstr = {'pid' : pid}
+                    err_msg = "Warning! Couldn't kill {pid}.\n".format(**formatstr)
+                    sys.stderr.write(err_msg)
+
+            time.sleep(1)
 
 
 def int_handler(signal, frame):
-    print('\nSIGINT captured. Exiting.')
+    sys.stdout.write('\nSIGINT captured. Exiting.\n')
     sys.exit(0)
 
 
 if __name__ == '__main__':
-	signal.signal(signal.SIGINT, int_handler)
-	main()
+    signal.signal(signal.SIGINT, int_handler)
+    main()
